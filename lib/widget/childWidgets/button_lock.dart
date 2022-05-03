56726dbd2a50efb430971aeb80570ef63ef7657d
cnvfirj <cnvfirj@gmail.com>
2022-05-03 16:19:11 +0300

    add loc

--- lib/widget/childWidgets/button_lock.dart
+++ lib/widget/childWidgets/button_lock.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+
 class LockKey extends StatelessWidget{
   const LockKey({Key? key}) : super(key: key);
 

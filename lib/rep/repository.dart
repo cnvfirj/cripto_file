e6bf52f67cc31a5a0920641f405d2ae59dbbbead
igor_ho <cnvfirj@gmail.com>
2022-07-04 13:04:27 +0300

    ongoing

--- lib/rep/repository.dart
+++ lib/rep/repository.dart
@@ -48,7 +48,7 @@ class RepositoryBlocKey implements PressLockKey,FieldTextNameKey,PressOtherButto
   dynamic getterNameKey;//извлекаем имя ключа
   @override
   void registerGetterNameKey(funk) {
-    // TODO: implement registerGetterNameKey
+    // TODO: implement registerGetterNameKey36345737373737djjgf
     getterNameKey = funk;
   }
 
